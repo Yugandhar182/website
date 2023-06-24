726ef60573c9de9673b75a6599fd48d4e785c823
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-06-24 08:23:46 +0530

    Update RecruitingAutomation.svelte

--- src/RecruitingAutomation.svelte
+++ src/RecruitingAutomation.svelte
@@ -12,10 +12,11 @@
         <h2 style="color: darkmagenta;">Recruiting Automation</h2>
         <p>Recruiting automation allows you to focus on the work that matters!</p>
       </div>
+      <h style="color:red; ">Why Automate</h>
         <section class="testimonials-section">
           <table>
           <div class="container">
-            <div class="row">
+            <div class="col">
               <div class="col-md-4">
                 <div class="feature-card">
                   <i class="fa fa-search"></i>
@@ -26,49 +27,81 @@
               <div class="col-md-4">
                 <div class="feature-card">
                   <i class="fa fa-envelope"></i>
-                  <h3 style="color:purple; font-weight:600;">Be in control</h3>
-                  <h6>Ensure that recruiting standards are enforced automatically, and build a streamlined and an error proof workflow.</h6>
+                  <h3 style="color:purple; font-weight:11600;">Proactive Interaction</h3>
+                  <h6>Using Automation + Email Sequences you can engage with your passive contacts and candidates..</h6>
                 </div>
               </div>
               <div class="col-md-4">
                 <div class="feature-card">
                   <i class="fa fa-users"></i>
-                  <h3 style="color:purple; font-weight:600;">Automate everything</h3>
-                  <h6>Automate mundane tasks so your team can focus on delivering smooth experience at every step of the recruiting cycle.</h6>
+                  <h3 style="color:purple; font-weight:600;">Contain Costs</h3>
+                  <h6>Automating redundant recruiting activities reduces the need for extensive labour.</h6>
                 </div>
               </div>
+              <div class="col-md-4">
+                <div class="feature-card">
+                  <i class="fa fa-users"></i>
+                  <h3 style="color:purple; font-weight:600;">Improve Consistency and Accuracy</h3>
+                  <h6>The possibility of errors with manual processing is a major drawback, especially when there are huge volumes involved.</h6>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="feature-card">
+                  <i class="fa fa-users"></i>
+                  <h3 style="color:purple; font-weight:600;">Happy Recruiters</h3>
+                  <h6>Executing recurring activities on a daily basis gets mundane and monotonous for employees.</h6>
+                </div>
+              </div>
+              
             
           </div>
         </table>
         </section>
-          <button class="close-button" on:click={closeComponent}>Close</button>
+         
       </div>
       
   </section>
+ 
   
   <style>
    
    .hero-section {
     text-align: center;
-    margin-bottom: 200px;
+    margin-top: -100px;
+    margin-bottom: 100px;
   }
-    .recruiting-automation-section h1 {
-      color: blue;
+  .recruiting-automation-section h{
+     
+     font-weight: bold;
+     font-size: 2rem;
+   }
+    .recruiting-automation-section h2 {
+        margin-top: 1px;
+    margin-bottom: 1px;
       font-weight: bold;
-      font-size: 2rem;
+      font-size: 3rem;
+    }
+    .recruiting-automation-section p {
+     
+      font-weight: bold;
+      font-size: 1.5rem;
     }
   
-   
-    .close-button {
-      margin-top: 20px;
-      padding: 10px 20px;
-      background-color: red;
-      color: white;
-      border: none;
-      border-radius: 5px;
-      font-size: 16px;
-      cursor: pointer;
+    .recruiting-automation-section h3 {
+
+      font-weight: bold;
+      font-size: 1.7rem;
     }
+    .recruiting-automation-section h6 {
+
+    font-weight: bold;
+    font-size: 1rem;
+   }
+ 
+
+  
+   
+    
     body {
       margin: 0;
       padding: 0;
@@ -77,7 +110,7 @@
 
     .automation-component {
       width: 100%;
-      height: 100vh;
+      height:150vh;
       display: flex;
       flex-direction: column;
       justify-content: center;
@@ -88,10 +121,7 @@
 
     
 
-    .automation-component p {
-      font-size: 18px;
-      margin-bottom: 10px;
-    }
+   
 
     .automation-component ul {
       list-style-type: disc;
@@ -99,7 +129,7 @@
     }
 
     .automation-component li {
-      font-size: 16px;
+      font-size: 10px;
       margin-bottom: 5px;
     }
   </style>
